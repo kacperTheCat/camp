b8ffbc3b8a21befd6e92abaa9f3f988d7ac37cbe
kacperTheCat <kacper.one.borowski@gmail.com>
2017-11-28 17:08:37 +0100

    new ex

---
+++ src/Input.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <p>{{msg}}</p>
+  <input type="text" :value="msg" @input="changeMessage">
+  <p>{{message}}</p>
+</div>
+</template>
+<script>
+export default {
+  props: ['msg'],
+  data() {
+    return {
+      message: ''
+    }
+  },methods: {
+    changeMessage(event) {
+      this.message = event.target.value;
+      this.$emit('messageChange', this.message);
+    }
+  }
+}
+</script>
+
+<style language="scss">
+input{
+  border: 2px solid red;
+  font-family: roboto;
+  }
+  p{
+    font-family: roboto;
+  }
+</style>
