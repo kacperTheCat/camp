b1ab357f5396a7d5e02607bfa205fb85a2b43b86
kacperTheCat <kacper.one.borowski@gmail.com>
2017-12-05 15:06:41 +0100

    lernign about vuex

---
+++ VUEX/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import {store} from './store.js'
+new Vue({
+  el: '#app',
+  store, //w es6 można nie pisac store: store, wystarczy skrot
+  render: h => h(App)
+})
